5cbb912bbdcd6efacaf114d3572e0827a8bf0996
Niran Babalola <niran@niran.org>
2016-04-13 06:03:48 -0500

    Add a no-op action so the reducer can be used for a portion of the state

--- src/store/action-type.js
+++ src/store/action-type.js
@@ -1,4 +1,5 @@
-import {createUnion} from 'redux-tcomb';
+import { createUnion } from 'redux-tcomb';
+import t from 'tcomb';
 import * as actions from './actions';
 
 
@@ -10,5 +11,24 @@ if (actionNames.length === 1) {
 } else {
   Action = createUnion(actions);
 }
+
+/**
+ * The NoOp action allows the reducer to ignore actions intended for a
+ * different portion of the state.
+ *
+ * https://github.com/gcanti/redux-tcomb/issues/7
+ */
+const NoOp = t.struct({});
+Object.assign(NoOp.prototype, {
+  patch(state) {
+    return state;
+  },
+});
+
+const strictDispatch = Action.dispatch;
+Action.dispatch = function (action) {
+  const isIdentityAction = actions.hasOwnProperty(action.type);
+  return isIdentityAction ? strictDispatch(action) : NoOp;
+};
 
 export default Action;
